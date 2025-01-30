ef7a43a42ff0d0ad3bf9522d22a1fbf97fcd7901
etleyden <etleyden@gmail.com>
2025-01-30 17:25:29 -0500

    Added readme, server.js, and JSON database

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,33 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello, world!" });
 });
 
+/*
+{
+    "email": 'email@domain.com',
+    "password": "password"
+}
+*/
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
 app.post("/api/create_user", (req, res) => {
-
+    console.log(req.body);
+    if(isValidEmail(req.body.email)) {
+        //check if email is already in the json
+        //write it to the JSON
+        res.send("User created!");
+    } else {
+        res.send("Invalid email!");
+    }
 });
 
 app.post("/api/create_recipe", (req, res) => {
-
+    console.log(res.body);
 });
 
 app.post("/api/add_product", (req, res) => {
-
+    console.log(res.body);
 });
 
 // Start the server
